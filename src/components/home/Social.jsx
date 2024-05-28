32e519bc3d5937880e54dc44ce26bd59833d7066
Eyepatch5263 <pratyushpragyey@gmail.com>
2024-05-28 23:45:50 +0530

    p

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/eyepatch_5263/" className="home__social-icon" target='_blank'>
+        <i class="bx bxl-instagram"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/pratyush-pragyey-7a95a7258/" className="home__social-icon" target='_blank'>
+        <i class="bx bxl-linkedin"></i>
+        </a>
+        <a href="https://github.com/Eyepatch5263" className="home__social-icon" target='_blank'>
+        <i class="bx bxl-github"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
